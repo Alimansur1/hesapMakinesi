da9b8f349c08a57d27af6d31449fa531287abf5d
Ali <alimansur_21@icloud.com>
2024-08-01 16:36:51 +0300

    Proje dosyası ekle.

---
+++ FaktorielHesap/FaktorielHesap.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sayi = 0;
+	int faktoriel = 1;
+
+
+	cout << "Sayi giriniz : ";
+	cin >> sayi;
+
+	for (int i = 1; i <= sayi; i++) {
+		faktoriel *= i;
+
+	}
+	cout << "Sonuc : " << faktoriel << endl;
+
+
+
+
+}
+
